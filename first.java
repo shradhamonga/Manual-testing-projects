2bd742a10b6459738d5e70cfd87ac647007f9281
shradhamonga <110901146+shradhamonga@users.noreply.github.com>
2025-10-15 20:09:47 +0530

    Add files via upload

---
+++ first.java
@@ -0,0 +1,23 @@
+
+public class first {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int math= 89;
+		int science=99;
+		int english= 79;
+		
+		int total = math + science+ english;
+		double avg= total /3;
+		
+		System.out.println("Total Marks:"+total+"\n Average Marks:"+avg);
+       
+        int averageInt= total /3;
+        System.out.println("New average after changing data type:"+averageInt);
+        
+        
+        
+	}
+
+}
